2d300227892417df67d00560d0be99517260f6b5
Takeshi Yaeda <t.yaeda@gmail.com>
2021-07-12 00:15:32 +0900

    refactor: move test fixture location

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 import test from "ava";
+import path from "path";
 import md2html from "../index.js";
 
+const fixtureBasePath = path.resolve("test", "fixtures");
+
 test("foo", async (t) => {
-  t.snapshot(await md2html("test.md"));
+  const mdPath = path.resolve(fixtureBasePath, "test.md");
+  t.snapshot(await md2html(mdPath));
 });
