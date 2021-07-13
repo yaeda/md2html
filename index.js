b3bafad21f4b30526bccddaf1e34f26e8a731982
Takeshi Yaeda <t.yaeda@gmail.com>
2021-07-13 09:16:46 +0900

    fix: correct css file path resolution

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import path, { dirname } from "path";
 import doc from "rehype-document";
 import minify from "rehype-preset-minify";
 import stringify from "rehype-stringify";
@@ -6,13 +7,20 @@ import gfm from "remark-gfm";
 import parse from "remark-parse";
 import mutate from "remark-rehype";
 import unified from "unified";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const cssPath = path.resolve(__dirname, "style.css");
 
 export default async (input) => {
   return unified()
     .use(parse)
     .use(gfm)
     .use(mutate)
-    .use(doc, { style: [fs.readFileSync("style.css", "utf8")] })
+    .use(doc, {
+      style: [fs.readFileSync(cssPath, "utf8")],
+    })
     .use(stringify)
     .use(minify)
     .process(fs.readFileSync(input))
